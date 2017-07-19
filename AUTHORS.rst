16950718d867222a7fd24f66cd0fa6f0b857a407
Abinash Meher <abinashdakshana999@gmail.com>
2017-07-19 21:46:46 +0530

    Initial commit

---
+++ AUTHORS.rst
@@ -0,0 +1,5 @@
+==========
+Developers
+==========
+
+* Abinash Meher <abinashdakshana999@gmail.com>
---
+++ tests/test_skeleton.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import pytest
+from srtvoiceext.skeleton import fib
+
+__author__ = "Abinash Meher"
+__copyright__ = "Abinash Meher"
+__license__ = "mit"
+
+
+def test_fib():
+    assert fib(1) == 1
+    assert fib(2) == 1
+    assert fib(7) == 13
+    with pytest.raises(AssertionError):
+        fib(-10)
